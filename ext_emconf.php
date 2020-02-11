d04cd2e372a3d8d434b73ecb3bb959527b630c2e
Stefan Froemken <froemken@gmail.com>
2020-02-11 09:34:00 +0100

    Update to version 3.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '3.0.1',
+    'version' => '3.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
-            'maps2' => '7.1.3-7.99.99'
+            'maps2' => '8.0.0-0.0.0'
         ],
         'conflicts' => [],
         'suggests' => []
