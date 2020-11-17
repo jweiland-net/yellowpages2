c25061f5be70a43872387686960879ae03d68769
Stefan Froemken <froemken@gmail.com>
2020-11-17 14:38:58 +0100

    Remove maps2 as hard requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,13 +13,13 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'maps2' => '8.0.0-0.0.0',
             'glossary2' => '4.0.0-4.99.99'
         ],
         'conflicts' => [
         ],
         'suggests' => [
-            'checkfaluploads' => ''
+            'checkfaluploads' => '',
+            'maps2' => '8.0.0-0.0.0'
         ]
     ],
 ];
