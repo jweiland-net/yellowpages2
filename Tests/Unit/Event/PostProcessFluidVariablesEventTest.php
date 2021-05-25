5a59f1fcd0fd8551aabdc727963f918197452c20
Stefan Froemken <froemken@gmail.com>
2021-05-25 13:23:08 +0200

    Add UnitTests for events and EventListeners

---
+++ Tests/Unit/Event/PostProcessFluidVariablesEventTest.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/yellowpages2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Yellowpages2\Tests\Unit\Event;
+
+use JWeiland\Yellowpages2\Event\PostProcessFluidVariablesEvent;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use TYPO3\CMS\Extbase\Mvc\Request;
+
+/**
+ * Test case.
+ */
+class PostProcessFluidVariablesEventTest extends UnitTestCase
+{
+    /**
+     * @var PostProcessFluidVariablesEvent
+     */
+    protected $subject;
+
+    /**
+     * @var Request|ObjectProphecy
+     */
+    protected $requestProphecy;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->requestProphecy = $this->prophesize(Request::class);
+        $this->requestProphecy
+            ->getControllerName()
+            ->willReturn('Company');
+        $this->requestProphecy
+            ->getControllerActionName()
+            ->willReturn('list');
+
+        $this->subject = new PostProcessFluidVariablesEvent(
+            $this->requestProphecy->reveal(),
+            [
+                'foo' => 'bar'
+            ],
+            [
+                'name' => 'Stefan Froemken'
+            ]
+        );
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->subject);
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function getRequestReturnsControllerRequest(): void
+    {
+        self::assertSame(
+            $this->requestProphecy->reveal(),
+            $this->subject->getRequest()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getControllerNameReturnsCompany(): void
+    {
+        self::assertSame(
+            'Company',
+            $this->subject->getControllerName()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getActionNameReturnsList(): void
+    {
+        self::assertSame(
+            'list',
+            $this->subject->getActionName()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getSettingsReturnsSettings(): void
+    {
+        self::assertSame(
+            ['foo' => 'bar'],
+            $this->subject->getSettings()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getFluidVariablesReturnsVariables(): void
+    {
+        self::assertSame(
+            ['name' => 'Stefan Froemken'],
+            $this->subject->getFluidVariables()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addFluidVariablesWillAddFluidVariable(): void
+    {
+        $this->subject->addFluidVariable('company', 'jweiland.net');
+        self::assertSame(
+            [
+                'name' => 'Stefan Froemken',
+                'company' => 'jweiland.net'
+            ],
+            $this->subject->getFluidVariables()
+        );
+    }
+}
---
+++ Tests/Unit/Event/PreProcessControllerActionEventTest.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/yellowpages2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Yellowpages2\Tests\Unit\Event;
+
+use JWeiland\Yellowpages2\Event\PostProcessFluidVariablesEvent;
+use JWeiland\Yellowpages2\Event\PreProcessControllerActionEvent;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use TYPO3\CMS\Extbase\Mvc\Controller\Arguments;
+use TYPO3\CMS\Extbase\Mvc\Request;
+
+/**
+ * Test case.
+ */
+class PreProcessControllerActionEventTest extends UnitTestCase
+{
+    /**
+     * @var PostProcessFluidVariablesEvent
+     */
+    protected $subject;
+
+    /**
+     * @var Request|ObjectProphecy
+     */
+    protected $requestProphecy;
+
+    /**
+     * @var Arguments|ObjectProphecy
+     */
+    protected $argumentsProphecy;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->requestProphecy = $this->prophesize(Request::class);
+        $this->requestProphecy
+            ->getControllerName()
+            ->willReturn('Company');
+        $this->requestProphecy
+            ->getControllerActionName()
+            ->willReturn('list');
+
+        $this->argumentsProphecy = $this->prophesize(Arguments::class);
+
+        $this->subject = new PreProcessControllerActionEvent(
+            $this->requestProphecy->reveal(),
+            $this->argumentsProphecy->reveal(),
+            [
+                'foo' => 'bar'
+            ]
+        );
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->subject);
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function getRequestReturnsControllerRequest(): void
+    {
+        self::assertSame(
+            $this->requestProphecy->reveal(),
+            $this->subject->getRequest()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getControllerNameReturnsCompany(): void
+    {
+        self::assertSame(
+            'Company',
+            $this->subject->getControllerName()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getActionNameReturnsList(): void
+    {
+        self::assertSame(
+            'list',
+            $this->subject->getActionName()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getArgumentsReturnsArguments(): void
+    {
+        self::assertSame(
+            $this->argumentsProphecy->reveal(),
+            $this->subject->getArguments()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getSettingsReturnsSettings(): void
+    {
+        self::assertSame(
+            ['foo' => 'bar'],
+            $this->subject->getSettings()
+        );
+    }
+}
