3aa78d17c79d51f124a689894ac80a7e06aeba8a
Stefan Froemken <froemken@gmail.com>
2022-05-09 12:26:25 +0200

    Use Prophecy Trait in tests

--- Tests/Unit/EventListener/AddGlossaryEventListenerTest.php
+++ Tests/Unit/EventListener/AddGlossaryEventListenerTest.php
@@ -17,6 +17,7 @@ use JWeiland\Yellowpages2\Event\PostProcessFluidVariablesEvent;
 use JWeiland\Yellowpages2\EventListener\AddGlossaryEventListener;
 use Nimut\TestingFramework\TestCase\UnitTestCase;
 use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 
@@ -25,6 +26,8 @@ use TYPO3\CMS\Core\Database\Query\QueryBuilder;
  */
 class AddGlossaryEventListenerTest extends UnitTestCase
 {
+    use ProphecyTrait;
+
     /**
      * @var AddGlossaryEventListener
      */
