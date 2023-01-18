d7ae9e15cbaace495f86e52a0442c269b3a17d25
Stefan Froemken <froemken@gmail.com>
2023-01-18 09:47:29 +0100

    Update version to 6.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '5.1.1',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.19-11.5.99',
+            'typo3' => '10.4.33-11.5.99',
             'glossary2' => '5.0.5-5.99.99'
         ],
         'conflicts' => [
