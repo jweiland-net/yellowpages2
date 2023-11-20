549d6c8feeb1f9dc49ec9ea2cedf586910638ad0
Stefan Froemken <sfroemken@jweiland.net>
2023-11-20 14:30:16 +0100

    Update query to select related categories of a company

--- Classes/Domain/Repository/CategoryRepository.php
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -11,13 +11,18 @@ declare(strict_types=1);
 
 namespace JWeiland\Yellowpages2\Domain\Repository;
 
+use JWeiland\Yellowpages2\Domain\Model\Company;
+use JWeiland\Yellowpages2\Domain\Traits\GetLanguageStatementTrait;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Query;
+use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
+use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Repository for TYPO3 categories
@@ -26,12 +31,24 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  */
 class CategoryRepository extends Repository
 {
+    use GetLanguageStatementTrait;
+
     /**
      * @var array
      */
     protected $defaultOrderings = [
         'title' => QueryInterface::ORDER_ASCENDING,
     ];
+
+    /**
+     * @var CompanyRepository
+     */
+    protected $companyRepository;
+
+    public function injectCompanyRepository(CompanyRepository $companyRepository): void
+    {
+        $this->companyRepository = $companyRepository;
+    }
 
     public function initializeObject(): void
     {
@@ -43,63 +60,85 @@ class CategoryRepository extends Repository
 
     /**
      * Needed for search partial to just list categories which have a company record assigned.
+     * There is no possibility to get all categories which are assigned to a company with extbase
+     * query (col "items" may contain various kinds of records). That's why we have to use the
+     * Doctrine QueryBuilder here.
      */
     public function findRelated(): QueryResultInterface
     {
         $query = $this->createQuery();
+        $query->getQuerySettings()->setRespectStoragePage(true);
 
-        $queryBuilder = $this->getConnectionPool()->getQueryBuilderForTable('sys_category');
-        $queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tx_yellowpages2_domain_model_company');
         $queryBuilder
-            ->select('sc.*')
-            ->from('sys_category', 'sc')
+            // Keep '*' for workspaces and translation
+            ->select('*')
+            ->from('tx_yellowpages2_domain_model_company', 'tx_yellowpages2_domain_model_company')
             ->leftJoin(
-                'sc',
+                'tx_yellowpages2_domain_model_company',
                 'sys_category_record_mm',
-                'sc_mm',
+                'sys_category_record_mm',
                 (string)$queryBuilder->expr()->andX(
                     $queryBuilder->expr()->eq(
-                        'sc_mm.tablenames',
+                        'sys_category_record_mm.tablenames',
                         $queryBuilder->createNamedParameter('tx_yellowpages2_domain_model_company')
                     ),
                     $queryBuilder->expr()->orX(
                         $queryBuilder->expr()->eq(
-                            'sc_mm.fieldname',
+                            'sys_category_record_mm.fieldname',
                             $queryBuilder->createNamedParameter('main_trade')
                         ),
                         $queryBuilder->expr()->eq(
-                            'sc_mm.fieldname',
+                            'sys_category_record_mm.fieldname',
                             $queryBuilder->createNamedParameter('trades')
                         ),
                     ),
                     $queryBuilder->expr()->eq(
-                        'sc_mm.uid_local',
-                        $queryBuilder->quoteIdentifier('sc.uid')
+                        'sys_category_record_mm.uid_foreign',
+                        $queryBuilder->quoteIdentifier('tx_yellowpages2_domain_model_company.uid')
                     )
                 )
             )
             ->leftJoin(
-                'sc_mm',
-                'tx_yellowpages2_domain_model_company',
-                'c',
+                'sys_category_record_mm',
+                'sys_category',
+                'sys_category',
                 $queryBuilder->expr()->eq(
-                    'sc_mm.uid_foreign',
-                    $queryBuilder->quoteIdentifier('c.uid')
+                    'sys_category_record_mm.uid_local',
+                    $queryBuilder->quoteIdentifier('sys_category.uid')
                 )
             )
             ->where(
                 $queryBuilder->expr()->isNotNull(
-                    'c.uid'
+                    'sys_category.uid'
+                ),
+                $queryBuilder->expr()->in(
+                    'tx_yellowpages2_domain_model_company.pid',
+                    $this->getQuerySettingsOfCompany()->getStoragePageIds()
                 )
             )
-            ->groupBy('sc.uid')
-            ->orderBy('sc.title');
+            ->andWhere(
+                $queryBuilder->expr()->orX(
+                    ...$this->getLanguageStatement(
+                        'tx_yellowpages2_domain_model_company',
+                    'tx_yellowpages2_domain_model_company',
+                        $this->getQuerySettingsOfCompany(),
+                        $queryBuilder
+                    )
+                )
+            )
+            ->orderBy('sys_category.title', 'ASC')
+            ->groupBy('sys_category.uid');
 
         return $query->statement($queryBuilder)->execute();
     }
 
-    protected function getConnectionPool(): ConnectionPool
+    /**
+     * Get QuerySettings for company records. Needed for translation and to select companies by requested PID.
+     */
+    protected function getQuerySettingsOfCompany(): QuerySettingsInterface
     {
-        return GeneralUtility::makeInstance(ConnectionPool::class);
+        return $this->companyRepository->createQuery()->getQuerySettings();
     }
 }
