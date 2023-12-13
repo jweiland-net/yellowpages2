e6ac33b5e264ce176206380000fcca80aee3fd4c
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-12-13 17:17:50 +0100

    Added fe_user class mappings to Persistence Classes

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -4,4 +4,7 @@ return [
     \JWeiland\Yellowpages2\Domain\Model\Category::class => [
         'tableName' => 'sys_category',
     ],
+    \JWeiland\Yellowpages2\Domain\Model\FeUser::class => [
+        'tableName' => 'fe_users',
+    ],
 ];
