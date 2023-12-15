61db91934c96cca3e9f6b0a15b1f83c7394c7578
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-12-15 14:07:09 +0100

    Fix with exception type and comments

--- Classes/Middleware/ControllerActionsMiddleware.php
+++ Classes/Middleware/ControllerActionsMiddleware.php
@@ -19,8 +19,16 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\Exception\MissingArrayPathException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
+/**
+ * This middleware handles the request variable for createAction [Company]
+ * 1. Remove empty trades from request to prevent errors while storing/updating the FE record
+ * 2. Files will be process by TypeConverter
+ * But, if an error occurs, we have to remove them.
+ * 3. Sanitize search keyword with htmlspecialchars
+ */
 class ControllerActionsMiddleware implements MiddlewareInterface
 {
     protected RequestFieldModifierInterface $modifier;
@@ -78,7 +86,7 @@ class ControllerActionsMiddleware implements MiddlewareInterface
             // Apply the modified request body to the request
             return $request->withParsedBody($requestBody);
 
-        } catch (\Exception $exception) {
+        } catch (MissingArrayPathException | \RuntimeException $exception) {
             // Do nothing inside the catch block
             return $request;
         }
