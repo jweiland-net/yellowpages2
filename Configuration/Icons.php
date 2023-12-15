3004260e98f47821c4db5c7ee627499a828360ac
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-12-15 17:57:04 +0100

    Conflict fix as rebase happened

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ext-yellowpages2-directory-wizard-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:yellowpages2/Resources/Public/Icons/plugin_wizard.svg'
+    ],
+];
