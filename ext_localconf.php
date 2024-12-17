593cac524d1d89d30a33b611ec0330045ea28130
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-12-17 17:17:10 +0100

    [TASK] Code formatting and optimizations done for ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,28 +1,34 @@
 <?php
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use JWeiland\Yellowpages2\Controller\CompanyController;
+use JWeiland\Yellowpages2\Controller\MapController;
+use JWeiland\Yellowpages2\Hook\ClearCacheHook;
+use JWeiland\Yellowpages2\Tasks\Update;
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
 call_user_func(static function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'yellowpages2',
         'Directory',
         [
-            \JWeiland\Yellowpages2\Controller\CompanyController::class => 'list, listMyCompanies, show, search, new, create, edit, update, activate',
-            \JWeiland\Yellowpages2\Controller\MapController::class => 'new, create, edit, update, activate',
+            CompanyController::class => 'list, listMyCompanies, show, search, new, create, edit, update, activate',
+            MapController::class => 'new, create, edit, update, activate',
         ],
         // non-cacheable actions
         [
-            \JWeiland\Yellowpages2\Controller\CompanyController::class => 'search, create, update, activate',
-            \JWeiland\Yellowpages2\Controller\MapController::class => 'create, update',
+            CompanyController::class => 'search, create, update, activate',
+            MapController::class => 'create, update',
         ]
     );
 
     // Clear cache of pages with yellowpages plugins, if a company record was edited/created/deleted in BE
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['yellowpages2_clearcache']
-        = \JWeiland\Yellowpages2\Hook\ClearCacheHook::class . '->clearCachePostProc';
+        = ClearCacheHook::class . '->clearCachePostProc';
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][JWeiland\Yellowpages2\Tasks\Update::class] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][Update::class] = [
         'extension' => 'yellowpages2',
         'title' => 'Inform users to update their company record',
         'description' => 'Hide company records which are older than 13 month and inform users to update their company record  after 12 month.',
