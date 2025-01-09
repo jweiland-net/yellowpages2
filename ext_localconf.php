338643f82b1e30ae7ce164b13d880aee6582f32a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-01-09 13:01:17 +0100

    [TASK] Removed old scheduler task configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,7 +10,6 @@
 use JWeiland\Yellowpages2\Controller\CompanyController;
 use JWeiland\Yellowpages2\Controller\MapController;
 use JWeiland\Yellowpages2\Hook\ClearCacheHook;
-use JWeiland\Yellowpages2\Tasks\Update;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 if (!defined('TYPO3')) {
@@ -36,10 +35,4 @@ call_user_func(static function () {
     // Clear cache of pages with yellowpages plugins, if a company record was edited/created/deleted in BE
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['yellowpages2_clearcache']
         = ClearCacheHook::class . '->clearCachePostProc';
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][Update::class] = [
-        'extension' => 'yellowpages2',
-        'title' => 'Inform users to update their company record',
-        'description' => 'Hide company records which are older than 13 month and inform users to update their company record  after 12 month.',
-    ];
 });
