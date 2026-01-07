94266a2cce2ad6027898926b1d59a8df329d5011
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-01-07 22:22:53 +0100

    [TASK] Updated Wizard name to [extension-key] format and updated prefixes

--- Classes/Updates/PluginToContentElementUpdate.php
+++ Classes/Updates/PluginToContentElementUpdate.php
@@ -29,7 +29,7 @@ class PluginToContentElementUpdate extends AbstractListTypeToCTypeUpdate
 
     public function getTitle(): string
     {
-        return 'Migrate plugins to Content Elements';
+        return '[yellowpages2] Migrate plugins to Content Elements';
     }
 
     public function getDescription(): string
