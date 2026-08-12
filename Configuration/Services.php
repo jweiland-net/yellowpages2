7e21a7e5c5cc1ff739e59a1dfa4de1a614f638c9
Stefan Froemken <sfroemken@jweiland.net>
2026-08-12 15:23:38 +0200

    [BUGFIX] Remove dead constructor argument in Services.php
    ModifierServiceProvider declares no __construct method and
    hardcodes the tag name 'yellowpages2.request.modifiers' in
    findTaggedServiceIds(). The argument passed to
    new ModifierServiceProvider(...) was therefore silently
    discarded by PHP and had no effect.

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -14,5 +14,5 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container, ContainerBuilder $containerBuilder) {
-    $containerBuilder->addCompilerPass(new ModifierServiceProvider('yellowpages2.request.modifiers'));
+    $containerBuilder->addCompilerPass(new ModifierServiceProvider());
 };
