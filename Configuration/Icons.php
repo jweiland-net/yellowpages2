087f661e3c3e948e40e9a80b892edfa30fa86f16
Stefan Froemken <sfroemken@jweiland.net>
2026-08-12 15:18:00 +0200

    [BUGFIX] Use import instead of FQCN in Icons.php
    Replace the fully qualified class name for SvgIconProvider with
    a use statement, in line with the project's coding standard of
    avoiding FQCN in the code body.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -9,9 +9,11 @@ declare(strict_types=1);
  * LICENSE file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'ext-yellowpages2-directory-wizard-icon' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:yellowpages2/Resources/Public/Icons/plugin_wizard.svg',
     ],
 ];
